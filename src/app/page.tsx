6347488e07c9a6698a37d5fe64f7b2af2ad9c07d
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2024-05-27 15:34:21 +0800

    ch2: setup upstash redis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Button from "@/components/ui/Button";
+import { db } from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  await db.set("hello", "hello");
   return (
     <div>
       <p>Hello World!</p>
